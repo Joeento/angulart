b12e8d761cdaae87f1bb8184f1c6ad46128238ce
Eric Kudler <joeento@gmail.com>
2015-11-19 16:42:18 -0500

    Added a canvas and the ability to display 2 connected line segments

--- client/app/string-art/string-art.controller.js
+++ client/app/string-art/string-art.controller.js
@@ -2,5 +2,39 @@
 
 angular.module('angulartApp')
   .controller('StringArtCtrl', function ($scope, $http) {
-    console.log('hey');
+	var Point = function (x, y) {
+		this.x = x;
+		this.y = y;
+	};
+
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+
+	var width = canvas.width;
+	var height = canvas.height;
+
+	function draw() {
+		for (var i = 0;i < $scope.points.length;i++) {
+			ctx.beginPath();
+			ctx.arc($scope.points[i].x, $scope.points[i].y, 5, 0, 3 * Math.PI, false);
+			ctx.fillStyle = '#ccddff';
+			ctx.fill();
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = '#666666';
+			ctx.stroke();
+			if (i > 0) {
+				ctx.beginPath();
+				ctx.moveTo($scope.points[i-1].x, $scope.points[i-1].y);
+				ctx.lineTo($scope.points[i].x, $scope.points[i].y);
+				ctx.stroke();
+			}
+		}
+	}
+
+	$scope.points = [];
+	for (var i = 0;i < 3; i++) {
+		$scope.points.push(new Point(Math.floor((Math.random()*width)), Math.floor((Math.random()*height))));
+	}
+	draw();
+
   });
